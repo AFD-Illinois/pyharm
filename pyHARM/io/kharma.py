e00390eda4c7b06fc69d864492b3577d53d3a88b
Ben Prather <bprathr2@illinois.edu>
2021-02-09 14:24:30 -0600

    Read more KHARMA output variables

--- pyHARM/io/kharma.py
+++ pyHARM/io/kharma.py
@@ -109,13 +109,18 @@ def read_dump(fname, add_ghosts=False, params=None):
         else:
             o = [None, None, None, None, None, None]
         # False == don't flatten into 1D array
-        #print("Bound ", b)
-        P[:, b[0]+ng_ix:b[1]+ng_ix, b[2]+ng_iy:b[3]+ng_iy, b[4]+ng_iz:b[5]+ng_iz] = \
-            f.Get('c.c.bulk.prims', False)[ib,o[4]:o[5],o[2]:o[3],o[0]:o[1],:].transpose(3,2,1,0)
+        prim_array = f.Get('c.c.bulk.prims', False)[ib,o[4]:o[5],o[2]:o[3],o[0]:o[1],:].transpose(3,2,1,0)
+        if prim_array.shape[0] == 8:
+            P[:, b[0]+ng_ix:b[1]+ng_ix, b[2]+ng_iy:b[3]+ng_iy, b[4]+ng_iz:b[5]+ng_iz] = prim_array
+        else:
+            P[:5, b[0]+ng_ix:b[1]+ng_ix, b[2]+ng_iy:b[3]+ng_iy, b[4]+ng_iz:b[5]+ng_iz] = prim_array
+            P[5:, b[0]+ng_ix:b[1]+ng_ix, b[2]+ng_iy:b[3]+ng_iy, b[4]+ng_iz:b[5]+ng_iz] = \
+                f.Get('c.c.bulk.B_prim', False)[ib,o[4]:o[5],o[2]:o[3],o[0]:o[1],:].transpose(3,2,1,0)
 
     return (P, params)
 
 def read_jcon(fname, add_ghosts=False):
+    # TODO take params and don't re-read etc
     f = phdf(fname)
 
     params = read_hdr(fname)
@@ -148,12 +153,9 @@ def read_jcon(fname, add_ghosts=False):
     else:
         ng_iz = ng_ix
 
-    params['startx1'] = G.startx[1]
-    params['startx2'] = G.startx[2]
-    params['startx3'] = G.startx[3]
-    params['dx1'] = dx = G.dx[1]
-    params['dx2'] = dy = G.dx[2]
-    params['dx3'] = dz = G.dx[3]
+    dx = G.dx[1]
+    dy = G.dx[2]
+    dz = G.dx[3]
 
     # Lay out the blocks and determine total mesh size
     bounds = []
@@ -186,3 +188,84 @@ def read_jcon(fname, add_ghosts=False):
             f.Get('c.c.bulk.jcon', False)[ib,o[4]:o[5],o[2]:o[3],o[0]:o[1],:].transpose(3,2,1,0)
 
     return jcon
+
+def read_psi(fname, add_ghosts=False):
+    return read_scalar(fname, 'c.c.bulk.psi_cd_prim', add_ghosts=add_ghosts)
+
+def read_divb(fname, add_ghosts=False):
+    return read_scalar(fname, 'c.c.bulk.divB', add_ghosts=add_ghosts)
+
+def read_fail_flags(fname, add_ghosts=False):
+    return read_scalar(fname, 'c.c.bulk.pflag', dtype=np.int32, add_ghosts=add_ghosts)
+
+def read_floor_flags(fname, add_ghosts=False):
+    return read_scalar(fname, 'c.c.bulk.fflag', dtype=np.int32, add_ghosts=add_ghosts)
+
+def read_scalar(fname, scalar_name, dtype=np.float64, add_ghosts=False):
+    f = phdf(fname)
+
+    params = read_hdr(fname)
+    G = Grid(params)
+
+    # First, quit if we've been asked for ghost zones but can't produce them
+    if add_ghosts and not f.IncludesGhost:
+        raise ValueError("Ghost zones aren't available in file {}".format(fname))
+
+    # This is the number of ghost zones present in the file
+    ng_f = f.IncludesGhost * f.NGhost
+
+    if add_ghosts:
+        params['ng'] = f.NGhost
+        # This is the number we should include on output
+        ng_ix = ng_f
+    else:
+        params['ng'] = 0
+        # This is the number we should include on output
+        ng_ix = 0
+
+    # Trivial dimensions don't have ghosts
+    if params['n2'] == 1:
+        ng_iy = 0
+    else:
+        ng_iy = ng_ix
+
+    if params['n3'] == 1:
+        ng_iz = 0
+    else:
+        ng_iz = ng_ix
+
+    dx = G.dx[1]
+    dy = G.dx[2]
+    dz = G.dx[3]
+
+    # Lay out the blocks and determine total mesh size
+    bounds = []
+    for ib in range(f.NumBlocks):
+        bb = f.BlockBounds[ib]
+        # Internal location of the block i.e. starting/stopping indices in the final, big mesh
+        bound = [int((bb[0]+dx/2 - G.startx[1])/dx)-ng_ix, int((bb[1]+dx/2 - G.startx[1])/dx)+ng_ix,
+                 int((bb[2]+dy/2 - G.startx[2])/dy)-ng_iy, int((bb[3]+dy/2 - G.startx[2])/dy)+ng_iy,
+                 int((bb[4]+dz/2 - G.startx[3])/dz)-ng_iz, int((bb[5]+dz/2 - G.startx[3])/dz)+ng_iz]
+        bounds.append(bound)
+
+    # Optionally allocate enough space for ghost zones
+    var = np.zeros((G.NTOT[1]+2*ng_ix, G.NTOT[2]+2*ng_iy, G.NTOT[3]+2*ng_iz))
+
+    # Read blocks into their slices of the full mesh
+    for ib in range(f.NumBlocks):
+        b = bounds[ib]
+        # Exclude ghost zones if we don't want them
+        if ng_ix == 0 and ng_f != 0:
+            if params['n2'] == 1:
+                o = [ng_f, -ng_f, None, None, None, None]
+            elif params['n3'] == 1:
+                o = [ng_f, -ng_f, ng_f, -ng_f, None, None]
+            else:
+                o = [ng_f, -ng_f, ng_f, -ng_f, ng_f, -ng_f]
+        else:
+            o = [None, None, None, None, None, None]
+        # False == don't flatten into 1D array
+        var[b[0]+ng_ix:b[1]+ng_ix, b[2]+ng_iy:b[3]+ng_iy, b[4]+ng_iz:b[5]+ng_iz] = \
+            f.Get(scalar_name, False)[ib,o[4]:o[5],o[2]:o[3],o[0]:o[1]].transpose(2,1,0)
+
+    return var
