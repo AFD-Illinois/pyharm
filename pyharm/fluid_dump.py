Fix divB calculation, more lenient movie options. Defaults to reading at double-precision

pyharm/fluid_dump.py
@@ -170,7 +170,8 @@ class FluidDump:
             if "flag" in key:
                 out = self.reader.read_var(key, astype=np.int32, slc=self.slice)
             else:
-                out = self.reader.read_var(key, astype=np.float32, slc=self.slice)
+                # TODO Option for double
+                out = self.reader.read_var(key, astype=np.float64, slc=self.slice)
             if out is None:
                 raise ValueError("FluidDump cannot find or compute {}".format(key))
             else:

pyharm/grmhd/b_field.py
@@ -10,17 +10,9 @@ def divB(G, B):
     gdet = G.gdet[Loci.CENT.value]
 
     # If we don't have ghost zones, make our own slices
-    if G.NG > 0:
-        s = G.slices
-    else:
-        s = Slices(ng=1)
+    s = Slices(ng=1)
 
     original_shape = B.shape
-
-    # Just replicate if we're in 2D    
-    if len(B.shape) < 4:
-        B = B[:, :, :, None].repeat(9, axis=-1)
-        gdet = gdet[:, :, None].repeat(7, axis=-1)
 
     divB = np.abs(0.25 * (
             B[0][s.b, s.b, s.b] * gdet[s.b, s.b, :]
@@ -50,13 +42,46 @@ def divB(G, B):
             - B[2][s.l1, s.b, s.l1] * gdet[s.l1, s.b, :]
             - B[2][s.l1, s.l1, s.l1] * gdet[s.l1, s.l1, :]
             ) / G.dx[3])
-    if G.NG > 0:
-        return divB
-    else:
-        divB_full = np.zeros(original_shape[1:])
-        if len(divB_full.shape) == 2:
-            divB_full[s.b, s.b] = divB[:, :, 3]
-        elif len(divB_full.shape) == 3:
-            divB_full[s.b, s.b, s.b] = divB
 
-        return divB_full
+    divB_full = np.zeros(original_shape[1:])
+    divB_full[s.b, s.b, s.b] = divB
+
+    return divB_full
+
+def divB_cons(G, B):
+
+    s = Slices(ng=1)
+
+    divB = np.abs(0.25 * (
+            B[0][s.b, s.b, s.b]
+            + B[0][s.b, s.l1, s.b]
+            + B[0][s.b, s.b, s.l1]
+            + B[0][s.b, s.l1, s.l1]
+            - B[0][s.l1, s.b, s.b]
+            - B[0][s.l1, s.l1, s.b]
+            - B[0][s.l1, s.b, s.l1]
+            - B[0][s.l1, s.l1, s.l1]
+            ) / G.dx[1] + 0.25 * (
+            B[1][s.b, s.b, s.b]
+            + B[1][s.l1, s.b, s.b]
+            + B[1][s.b, s.b, s.l1]
+            + B[1][s.l1, s.b, s.l1]
+            - B[1][s.b, s.l1, s.b]
+            - B[1][s.l1, s.l1, s.b]
+            - B[1][s.b, s.l1, s.l1]
+            - B[1][s.l1, s.l1, s.l1]
+            ) / G.dx[2] + 0.25 * (
+            B[2][s.b, s.b, s.b]
+            + B[2][s.b, s.l1, s.b]
+            + B[2][s.l1, s.b, s.b]
+            + B[2][s.l1, s.l1, s.b]
+            - B[2][s.b, s.b, s.l1]
+            - B[2][s.b, s.l1, s.l1]
+            - B[2][s.l1, s.b, s.l1]
+            - B[2][s.l1, s.l1, s.l1]
+            ) / G.dx[3])
+
+    divB_full = np.zeros(B.shape[1:])
+    divB_full[s.b, s.b, s.b] = divB
+
+    return divB_full

pyharm/plots/frame.py
@@ -19,7 +19,7 @@ from .pretty import pretty
 """Generate one frame of a movie.  Currently pretty useless outside `pyharm-movie` script,
 included in pyharm so as to be imported there easily.
 
-The code in `figures` would be a better place to start in writing
+The code in `figures` would be a better place to start in writing your own additional movies/plots.
 """
 
 def frame(fname, diag, kwargs):
@@ -157,10 +157,14 @@ def frame(fname, diag, kwargs):
             if "_poloidal" in movie_type or "_2d" in movie_type:
                 ax = plt.subplot(1, 1, 1)
                 var = movie_type.replace("_poloidal","")
+                if "divB" in var:
+                    var = dump[var]
                 plot_xz(ax, dump, var, **plotrc)
             elif "_toroidal" in movie_type:
                 ax = plt.subplot(1, 1, 1)
                 var = movie_type.replace("_toroidal","")
+                if "divB" in var:
+                    var = dump[var]
                 plot_xy(ax, dump, var, **plotrc)
             elif "_1d" in movie_type:
                 ax = plt.subplot(1, 1, 1)
@@ -175,6 +179,8 @@ def frame(fname, diag, kwargs):
                 ax = ax_slc[0]
                 var = movie_type
                 #print("Plotting slices. plotrc: ", plotrc)
+                if "divB" in var:
+                    var = dump[var]
                 plot_slices(ax_slc[0], ax_slc[1], dump, var, **plotrc) # We'll plot the field ourselves
 
             if no_margin or "jsq" in movie_type:
@@ -192,7 +198,16 @@ def frame(fname, diag, kwargs):
         if fig._suptitle is None or fig._suptitle.get_text() == "":
             if "divB" in movie_type:
                 # Special title for diagnostic divB
-                fig.suptitle(r"Max $\nabla \cdot B$ = {}".format(np.max(np.abs(dump['divB']))))
+                if "con" in movie_type:
+                    divb = np.abs(dump['divB_con'])
+                    divb_max = np.max(divb)
+                    divb_argmax = np.argmax(divb)
+                else:
+                    divb = np.abs(dump['divB'])
+                    divb_max = np.max(divb)
+                    divb_argmax = np.argmax(divb)
+                fig.suptitle(r"Max $\nabla \cdot B$ = {}".format(divb_max))
+                print("divB max", divb_max, "at", np.unravel_index(divb_argmax, divb.shape))
             else:
                 # Title by time, otherwise number
                 fig.suptitle("t = {}".format(int(tdump)))