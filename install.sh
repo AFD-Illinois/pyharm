e04f2596d05c3f9d2081336f6bb47a41920db325
Ben Prather <bprathr2@illinois.edu>
2023-09-26 09:47:12 -0600

    Add prereqs/installer/version for new release soon^TM

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-git submodule update --recursive --init
+if [ command -v git > /dev/null 2>&1 ]; then
+  git submodule update --recursive --init
+fi
 
 # Library and set management
 cd external/hallmark
--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ write_git_revision("pyharm")
 
 
 setup(name="pyharm",
-      version="2022.8",
+      version="2023.9",
       description="Python tools for HARM analysis",
       long_description=open("README.md", "rt").read(),
       long_description_content_type='text/markdown',
